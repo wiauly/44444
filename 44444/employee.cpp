e8929e8e19931c5d9861b17b40ebdc151f8e95ff
aziza <hhaziza0@gmail.com>
2023-08-18 19:25:10 +0200

    реальзация класса employee.cpp

--- 44444/employee.cpp
+++ 44444/employee.cpp
@@ -1,1 +1,27 @@
 #include "employee.h"
+
+employee::employee()
+{
+    name = "Noname";
+    position = "Noposition";
+    salary = 0;
+}
+
+employee::employee(string name, string posotion, float salary)
+{
+    this->name = name;
+    this->position = position;
+    this->salary = salary;
+}
+
+void employee::show() const
+{
+    cout << "Name:" << name << endl;
+    cout << "Position:" << position << endl;
+    cout << "Salary:" << salary << "uah" << endl;
+}
+
+float employee::calcSalary() const
+{
+    return salary;
+}
